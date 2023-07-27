a11a17f039c70083c2fba42d498513c78cbfd126
oluwakemiegbinola <oluwakemiegbinola@yahoo.com>
2023-07-27 02:42:38 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/1-main.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	list_t *head;
+	list_t *new;
+	list_t hello = {"World", 5, NULL};
+	size_t n;
+
+	head = &hello;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->str = strdup("Hello");
+	new->len = 5;
+	new->next = head;
+	head = new;
+	n = list_len(head);
+	printf("-> %lu elements\n", n);
+	free(new->str);
+	free(new);
+	return (0);
+}
+julien@ubuntu:~/0x12. Singly linked lists$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-list_len.c -o b
+julien@ubuntu:~/0x12. Singly linked lists$ ./b 
+-> 2 elements
+julien@ubuntu:~/0x12. Singly linked lists$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x12-singly_linked_lists
+File: 1-list_len.c
+
+2. Add node
+mandatory
+Write a function that adds a new node at the beginning of a list_t list.
+
+Prototype: list_t *add_node(list_t **head, const char *str);
+Return: the address of the new element, or NULL if it failed
+str needs to be duplicated
+You are allowed to use strdup
+julien@ubuntu:~/0x12. Singly linked lists$ cat 2-main.c
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	list_t *head;
+
+	head = NULL;
+	add_node(&head, "Alexandro");
+	add_node(&head, "Asaia");
+	add_node(&head, "Augustin");
+	add_node(&head, "Bennett");
+	add_node(&head, "Bilal");
+	add_node(&head, "Chandler");
+	add_node(&head, "Damian");
+	add_node(&head, "Daniel");
+	add_node(&head, "Dora");
+	add_node(&head, "Electra");
+	add_node(&head, "Gloria");
+	add_node(&head, "Joe");
+	add_node(&head, "John");
+	add_node(&head, "John");
+	add_node(&head, "Josquin");
+	add_node(&head, "Kris");
+	add_node(&head, "Marine");
+	add_node(&head, "Mason");
+	add_node(&head, "Praylin");
+	add_node(&head, "Rick");
+	add_node(&head, "Rick");
+	add_node(&head, "Rona");
+	add_node(&head, "Siphan");
+	add_node(&head, "Sravanthi");
+	add_node(&head, "Steven");
+	add_node(&head, "Tasneem");
+	add_node(&head, "William");
+	add_node(&head, "Zee");
+	print_list(head);
+	return (0);
+}
